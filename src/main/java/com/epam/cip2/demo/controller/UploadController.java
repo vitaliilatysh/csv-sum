735093cb2ce780ce8b7c23b576989785da584009
Vitalii_Latysh <vitalii_latysh@epam.com>
2019-04-25 10:42:30 +0300

    Optimized imports

--- src/main/java/com/epam/cip2/demo/controller/UploadController.java
+++ src/main/java/com/epam/cip2/demo/controller/UploadController.java
@@ -2,23 +2,11 @@ package com.epam.cip2.demo.controller;
 
 import com.epam.cip2.demo.service.FileService;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
-import java.util.Objects;
 
 @RestController
 public class UploadController {
